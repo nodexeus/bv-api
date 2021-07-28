6707b01cf9c7366aa3d08d534ae07a62cdbeac75
Chris Bruce <chris@chrisbruce.com>
2021-07-28 08:26:06 +0200

    adds transferred_At to vals

---
+++ migrations/20210728062111_transfer.down.sql
@@ -0,0 +1,2 @@
+-- Add down migration script here
+alter table validators drop column transferred_at;
---
+++ migrations/20210728062111_transfer.up.sql
@@ -0,0 +1,2 @@
+-- Add up migration script here
+alter table validators add column transferred_at timestamptz;
