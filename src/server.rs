ec6af7c17f87634fe377d9f3920808ee9d3d99ad
Chris Bruce <chris@chrisbruce.com>
2022-01-23 21:57:02 +0100

    adds better connection handling limits

--- src/server.rs
+++ src/server.rs
@@ -15,6 +15,7 @@ use serde::Deserialize;
 use sqlx::postgres::{PgPool, PgPoolOptions};
 use std::borrow::Cow;
 use std::str::FromStr;
+use std::time::Duration;
 use uuid::Uuid;
 
 type ApiResponse = errors::Result<HttpResponse>;
@@ -79,16 +80,25 @@ impl FromRequest for Authentication {
 
 pub async fn start() -> anyhow::Result<()> {
     let db_url = std::env::var("DATABASE_URL").expect("Missing DATABASE_URL");
-    let db_max_conn = std::env::var("DB_MAX_CONN")
+
+    let db_max_conn: u32 = std::env::var("DB_MAX_CONN")
         .unwrap_or("10".to_string())
         .parse()
         .unwrap();
+    let db_min_conn: u32 = std::env::var("DB_MIN_CONN")
+        .unwrap_or("2".to_string())
+        .parse()
+        .unwrap();
+
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
     let bind_ip = std::env::var("BIND_IP").unwrap_or("0.0.0.0".to_string());
     let addr = format!("{}:{}", bind_ip, port);
 
     let db_pool = PgPoolOptions::new()
         .max_connections(db_max_conn)
+        .min_connections(db_min_conn)
+        .max_lifetime(Some(Duration::from_secs(60 * 60 * 24)))
+        .idle_timeout(Some(Duration::from_secs(60 * 2)))
         .connect(&db_url)
         .await
         .expect("Could not create db connection pool.");
