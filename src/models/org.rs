3bc432c46de487bd767786e79a86b4e443f38ebe
Chris Bruce <chris@chrisbruce.com>
2022-05-18 18:58:17 +0200

    update povisions route order

--- src/models/org.rs
+++ src/models/org.rs
@@ -1,6 +1,7 @@
+use crate::errors::{ApiError, Result};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
+use sqlx::{FromRow, PgPool};
 use uuid::Uuid;
 
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize, sqlx::Type)]
@@ -12,13 +13,38 @@ pub enum OrgRole {
     Member,
 }
 
-#[derive(Debug, Serialize, Deserialize, FromRow)]
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
 pub struct Org {
     pub id: Uuid,
     pub name: String,
     pub is_personal: bool,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
+}
+
+impl Org {
+    pub async fn find_all_by_user_id(id: &Uuid, pool: &PgPool) -> Result<Vec<Org>> {
+        sqlx::query_as::<_, Self>(
+            r##"
+            SELECT 
+                orgs.* 
+            FROM 
+                orgs 
+            INNER JOIN 
+                orgs_users 
+            ON 
+                orgs.id = orgs_users.org_id 
+            WHERE 
+                orgs_users.user_id = $1
+            ORDER BY
+                lower(orgs.name)
+            "##,
+        )
+        .bind(&id)
+        .fetch_all(pool)
+        .await
+        .map_err(ApiError::from)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
