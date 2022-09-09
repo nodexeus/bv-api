51eadd567ffa1e723d74a37956bbd775e881ac3f
tstaetter <thomas.staetter@gmail.com>
2022-09-09 13:54:24 +0200

    fix: made user create unauthenticated
    as requested in https://github.com/blockjoy/blockvisor-api/issues/58

--- tests/grpc_user_service.rs
+++ tests/grpc_user_service.rs
@@ -64,7 +64,7 @@ async fn responds_unauthenticated_without_valid_token_for_get() {
 
 #[before(call = "setup")]
 #[tokio::test]
-async fn responds_ok_with_valid_token_for_create() {
+async fn responds_ok_without_token_for_create() {
     let db = Arc::new(_before_values.await);
     let request_meta = RequestMeta {
         id: Some(GrpcUuid::from(Uuid::new_v4())),
@@ -72,8 +72,6 @@ async fn responds_ok_with_valid_token_for_create() {
         fields: vec![],
         limit: None,
     };
-    let user = get_admin_user(&db.clone()).await;
-    let token = user.get_token(&db).await.unwrap();
     let grpc_user = GrpcUser {
         id: None,
         email: Some("hugo@boss.com".to_string()),
@@ -88,49 +86,9 @@ async fn responds_ok_with_valid_token_for_create() {
         password_confirmation: "abcde12345".to_string(),
         user: Some(grpc_user),
     };
-    let mut request = Request::new(inner);
-
-    request.metadata_mut().insert(
-        "authorization",
-        format!("Bearer {}", token.to_base64()).parse().unwrap(),
-    );
+    let request = Request::new(inner);
 
     assert_grpc_request! { create, request, tonic::Code::Ok, db, UserServiceClient<Channel> };
-}
-
-#[before(call = "setup")]
-#[tokio::test]
-async fn responds_unauthenticated_with_invalid_token_for_create() {
-    let db = Arc::new(_before_values.await);
-    let request_meta = RequestMeta {
-        id: Some(GrpcUuid::from(Uuid::new_v4())),
-        token: None,
-        fields: vec![],
-        limit: None,
-    };
-    let token = encode("some-invalid-token");
-    let grpc_user = GrpcUser {
-        id: None,
-        email: Some("hugo@boss.com".to_string()),
-        first_name: None,
-        last_name: None,
-        created_at: None,
-        updated_at: None,
-    };
-    let inner = CreateUserRequest {
-        meta: Some(request_meta),
-        password: "abcde12345".to_string(),
-        password_confirmation: "abcde12345".to_string(),
-        user: Some(grpc_user),
-    };
-    let mut request = Request::new(inner);
-
-    request.metadata_mut().insert(
-        "authorization",
-        format!("Bearer {}", token).parse().unwrap(),
-    );
-
-    assert_grpc_request! { create, request, tonic::Code::Unauthenticated, db, UserServiceClient<Channel> };
 }
 
 #[before(call = "setup")]
