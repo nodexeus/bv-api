d2ebedde3063b862f4db3e4bacdd7560c9484081
tstaetter <thomas.staetter@gmail.com>
2022-12-29 15:14:30 +0100

    feat: impl invitation service

--- src/grpc/ui_invitation_service.rs
+++ src/grpc/ui_invitation_service.rs
@@ -1,24 +1,25 @@
+use crate::auth::{FindableById, InvitationToken};
+use crate::errors::ApiError;
 use crate::grpc::blockjoy_ui::invitation_service_server::InvitationService;
 use crate::grpc::blockjoy_ui::{
-    CreateInvitationRequest, CreateInvitationResponse, InvitationRequest, InvitationsResponse,
-    ListPendingInvitationRequest, ListReceivedInvitationRequest,
+    CreateInvitationRequest, CreateInvitationResponse, Invitation as GrpcInvitation,
+    InvitationRequest, InvitationsResponse, ListPendingInvitationRequest,
+    ListReceivedInvitationRequest, ResponseMeta,
 };
-use crate::grpc::helpers::required;
-use crate::grpc::notification::ChannelNotifier;
+use crate::grpc::helpers::try_get_token;
 use crate::grpc::{get_refresh_token, response_with_refresh_token};
-use crate::models::Invitation;
+use crate::models::{Invitation, User};
 use crate::server::DbPool;
-use std::sync::Arc;
 use tonic::{Request, Response, Status};
+use uuid::Uuid;
 
 pub struct InvitationServiceImpl {
     db: DbPool,
-    notifier: Arc<ChannelNotifier>,
 }
 
 impl InvitationServiceImpl {
-    pub fn new(db: DbPool, notifier: Arc<ChannelNotifier>) -> Self {
-        Self { db, notifier }
+    pub fn new(db: DbPool) -> Self {
+        Self { db }
     }
 }
 
@@ -28,40 +29,105 @@ impl InvitationService for InvitationServiceImpl {
         &self,
         request: Request<CreateInvitationRequest>,
     ) -> Result<Response<CreateInvitationResponse>, Status> {
-        todo!()
+        let refresh_token = get_refresh_token(&request);
+        let inner = request.into_inner();
+        let invitation = GrpcInvitation {
+            created_by_id: None,
+            created_for_org_id: Some(inner.created_for_org_id),
+            invitee_email: Some(inner.invitee_email),
+            created_at: None,
+            accepted_at: None,
+            declined_at: None,
+        };
+
+        Invitation::create(&invitation, &self.db).await?;
+
+        let response_meta = ResponseMeta::from_meta(inner.meta);
+        let response = CreateInvitationResponse {
+            meta: Some(response_meta),
+        };
+
+        Ok(response_with_refresh_token(refresh_token, response)?)
     }
 
     async fn list_pending(
         &self,
         request: Request<ListPendingInvitationRequest>,
     ) -> Result<Response<InvitationsResponse>, Status> {
-        todo!()
+        let refresh_token = get_refresh_token(&request);
+        let inner = request.into_inner();
+        let org_id = Uuid::parse_str(inner.org_id.as_str()).map_err(ApiError::from)?;
+        let invitations: Vec<GrpcInvitation> = Invitation::pending(org_id, &self.db)
+            .await?
+            .iter()
+            .map(|i| i.try_into().unwrap_or_default())
+            .collect();
+
+        let response_meta = ResponseMeta::from_meta(inner.meta);
+        let response = InvitationsResponse {
+            meta: Some(response_meta),
+            invitations,
+        };
+
+        Ok(response_with_refresh_token(refresh_token, response)?)
     }
 
+    /// TODO: Currently users can list received invitations for other users by guessing the ID
     async fn list_received(
         &self,
         request: Request<ListReceivedInvitationRequest>,
     ) -> Result<Response<InvitationsResponse>, Status> {
-        todo!()
+        let refresh_token = get_refresh_token(&request);
+        let inner = request.into_inner();
+        let user_id = Uuid::parse_str(inner.user_id.as_str()).map_err(ApiError::from)?;
+        let email = User::find_by_id(user_id, &self.db).await?.email;
+        let invitations: Vec<GrpcInvitation> = Invitation::received(email, &self.db)
+            .await?
+            .iter()
+            .map(|i| i.try_into().unwrap_or_default())
+            .collect();
+        let response_meta = ResponseMeta::from_meta(inner.meta);
+        let response = InvitationsResponse {
+            meta: Some(response_meta),
+            invitations,
+        };
+
+        Ok(response_with_refresh_token(refresh_token, response)?)
     }
 
     async fn accept(&self, request: Request<InvitationRequest>) -> Result<Response<()>, Status> {
-        let refresh_token = get_refresh_token(&request);
-        let inner = request.into_inner();
-        let invitation = inner
-            .invitation
-            .ok_or_else(|| Status::invalid_argument("Invitation missing"))?;
+        let token = try_get_token::<_, InvitationToken>(&request)?;
+        let invitation_id = *token.id();
 
-        // Invitation::accept(invitation.&self.db).await?;
+        Invitation::accept(invitation_id, &self.db).await?;
 
-        Ok(response_with_refresh_token::<()>(refresh_token, ())?)
+        Ok(response_with_refresh_token::<()>(
+            get_refresh_token(&request),
+            (),
+        )?)
     }
 
     async fn decline(&self, request: Request<InvitationRequest>) -> Result<Response<()>, Status> {
-        todo!()
+        let token = try_get_token::<_, InvitationToken>(&request)?;
+        let invitation_id = *token.id();
+
+        Invitation::decline(invitation_id, &self.db).await?;
+
+        Ok(response_with_refresh_token::<()>(
+            get_refresh_token(&request),
+            (),
+        )?)
     }
 
     async fn revoke(&self, request: Request<InvitationRequest>) -> Result<Response<()>, Status> {
-        todo!()
+        let token = try_get_token::<_, InvitationToken>(&request)?;
+        let invitation_id = *token.id();
+
+        Invitation::revoke(invitation_id, &self.db).await?;
+
+        Ok(response_with_refresh_token::<()>(
+            get_refresh_token(&request),
+            (),
+        )?)
     }
 }
