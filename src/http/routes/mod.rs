4c4c747d504b42473b2ce9f4496ce3f8531dfca9
tstaetter <thomas.staetter@gmail.com>
2023-02-24 13:11:49 +0100

    feat: initial commit

--- src/http/routes/mod.rs
+++ src/http/routes/mod.rs
@@ -1,7 +1,45 @@
 use crate::http::handlers::health;
-use axum::routing::get;
+use axum::extract::Json;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
 use axum::Router;
+use serde::Deserialize;
 
 pub fn unauthenticated_routes() -> Router {
-    Router::new().route("/health", get(health))
+    let mqtt_router = Router::new()
+        // mqtt/auth
+        // -> { username: %u => JWT TOKEN }
+        .route("/auth", post(mqtt_auth))
+        .route("/acl", post(mqtt_acl));
+
+    Router::new()
+        .route("/health", get(health))
+        .nest("/mqtt", mqtt_router)
+}
+
+pub async fn mqtt_auth(Json(_payload): Json<MqttAuthRequest>) -> impl IntoResponse {
+    (StatusCode::NO_CONTENT, Json("{}"))
+}
+pub async fn mqtt_acl(Json(_payload): Json<MqttAclRequest>) -> impl IntoResponse {
+    (StatusCode::NO_CONTENT, Json("{}"))
+}
+
+#[derive(Deserialize)]
+pub struct MqttAuthRequest {
+    pub username: String,
+}
+
+#[derive(Deserialize, Eq, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum MqttOperationType {
+    Publish,
+    Subscribe,
+}
+
+#[derive(Deserialize)]
+pub struct MqttAclRequest {
+    pub operation: MqttOperationType,
+    pub username: String,
+    pub topic: Option<String>,
 }
