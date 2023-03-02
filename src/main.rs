cd026a1a46c2867213ed68e522d360df7496713a
Luuk Wester <luuk.wester@gmail.com>
2023-03-02 18:05:41 +0100

    Fix/mqtt (#245)
    * less noisy logs + smoke test + different status codes
    
    * generate client id instead of loading it from env
    
    * added more logging to acl endpoint
    
    * disable color
    
    ---------
    
    Co-authored-by: Luuk Wester <luuk@blockjoy.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     tracing_subscriber::registry()
-        .with(fmt::layer())
+        .with(fmt::layer().with_ansi(false))
         .with(EnvFilter::from_default_env())
         .init();
 
