a1ebdadc289694cd0d4de699abe86e6260f06420
Luuk Wester <luuk@blockjoy.com>
2023-03-06 17:17:14 +0100

    remove test message

--- src/grpc/mod.rs
+++ src/grpc/mod.rs
@@ -110,21 +110,6 @@ pub async fn server(
     let notifier = Notifier::new()
         .await
         .expect("Could not set up MQTT notifier!");
-    let msg = blockjoy::Command {
-        r#type: Some(blockjoy::command::Type::Node(blockjoy::NodeCommand {
-            node_id: uuid::Uuid::new_v4().to_string(),
-            api_command_id: uuid::Uuid::new_v4().to_string(),
-            created_at: Some(convert::try_dt_to_ts(chrono::Utc::now()).unwrap()),
-            host_id: uuid::Uuid::new_v4().to_string(),
-            command: None,
-        })),
-    };
-    notifier
-        .bv_commands_sender()
-        .unwrap()
-        .send(&msg)
-        .await
-        .unwrap();
 
     let discovery_service =
         grpc::blockjoy::discovery_server::DiscoveryServer::new(DiscoveryServiceImpl::default());
