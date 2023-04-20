2af00534b0b164333251bb76c820d4c4eebb5068
tstaetter <thomas.staetter@gmail.com>
2023-04-20 07:26:47 +0200

    feat: validating IPs

--- src/firewall.rs
+++ src/firewall.rs
@@ -3,6 +3,8 @@ use crate::models::Node;
 use crate::Error;
 use crate::Result as ApiResult;
 use anyhow::anyhow;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
 
 pub fn create_rule_for_node(node: &Node) -> ApiResult<Vec<Rule>> {
     let mut rules: Vec<Rule> = vec![];
@@ -22,31 +24,37 @@ pub fn create_rule_for_node(node: &Node) -> ApiResult<Vec<Rule>> {
 }
 
 fn create_firewall_rule(
+    // I'll leave the Vec for now, maybe we need it later
     denied_or_allowed_ips: &Vec<serde_json::Value>,
     action: Action,
 ) -> ApiResult<Rule> {
-    let mut ips: Vec<String> = vec![];
+    let mut final_ip = String::new();
 
     for ip in denied_or_allowed_ips {
         let ip = ip
             .as_object()
             .ok_or_else(|| Error::UnexpectedError(anyhow!("Unknown IP object")))?;
-
-        ips.push(String::from(
+        let ip = String::from(
             ip.get("ip")
                 .ok_or_else(|| Error::UnexpectedError(anyhow!("Invalid IP format")))?
                 .as_str()
                 .unwrap_or_default(),
-        ));
+        );
+
+        // Validate IP
+        if ip.contains("/") {
+            final_ip = cidr::Ipv4Cidr::from_str(ip.as_str())?.to_string();
+        } else {
+            final_ip = Ipv4Addr::from_str(ip.as_str())?.to_string();
+        };
     }
-    let ips = ips.join(",");
 
     Ok(Rule {
         name: "".to_string(),
         action: action as i32,
         direction: Direction::In as i32,
         protocol: None,
-        ips: Some(ips),
+        ips: Some(final_ip),
         ports: vec![],
     })
 }
