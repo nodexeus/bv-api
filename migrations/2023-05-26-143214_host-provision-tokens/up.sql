d5e9e5d35857c4dc84296295d011f7ff28dc6ec7
Luuk Wester <luuk.wester@gmail.com>
2023-05-26 17:39:12 +0200

    remove host_provisions, move over to host provision tokens

---
+++ migrations/2023-05-26-143214_host-provision-tokens/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+
+DROP TABLE host_provisions;
+
+ALTER TABLE orgs_users ADD COLUMN host_provision_token VARCHAR(32) NULL;
+UPDATE orgs_users SET host_provision_token = substr(md5(random()::text), 0, 12);
+ALTER TABLE orgs_users ALTER COLUMN host_provision_token SET NOT NULL;
