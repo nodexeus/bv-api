3eac9435c86a8c21a3b6cf81b2d0e8bc19e739f0
Luuk Wester <luuk.wester@gmail.com>
2023-07-12 02:43:40 +0200

    undo the damage

--- src/grpc/commands.rs
+++ src/grpc/commands.rs
@@ -86,44 +86,11 @@ async fn ack(
     let claims = conn.claims(&req, Endpoint::CommandAck).await?;
     let req = req.into_inner();
     let command = models::Command::find_by_id(req.id.parse()?, conn).await?;
-    println!("this claims: {claims:?} is going to ack this command: {command:?}");
-
     let host = command.host(conn).await?;
-    let node: Option<models::Node> = command.node(conn).await?;
-    let reason = match claims.resource() {
-        Resource::User(user_id) => {
-            if let Some(node) = &node {
-                (!models::Org::is_member(user_id, node.org_id, conn).await?)
-                    .then_some("user not a member")
-            } else {
-                (!models::Org::is_member(user_id, host.org_id, conn).await?)
-                    .then_some("user not a member")
-            }
-        }
-        Resource::Org(org_id) => {
-            if let Some(node) = dbg!(&node) {
-                (org_id != node.org_id).then_some("org incorrect node id")
-            } else {
-                (org_id != host.org_id).then_some("org incorrect host id")
-            }
-        }
-        Resource::Host(host_id) => {
-            if let Some(node) = dbg!(&node) {
-                (host_id != node.host_id).then_some("host incorrect node host id")
-            } else {
-                (host_id != host.id).then_some("host incorrect node id")
-            }
-        }
-        Resource::Node(node_id) => {
-            if let Some(node) = &node {
-                (node_id != node.id).then_some("node incorrect id")
-            } else {
-                None
-            }
-        }
-    };
-    if let Some(reason) = reason {
-        super::forbidden!("Access denied for command ack of {}: `{reason}`", req.id);
+    let node = command.node(conn).await?;
+    let is_allowed = access_allowed(claims, node.as_ref(), &host, conn).await?;
+    if !is_allowed {
+        super::forbidden!("Access denied for command ack of {}", req.id);
     }
     if command.acked_at.is_none() {
         command.ack(conn).await?;
