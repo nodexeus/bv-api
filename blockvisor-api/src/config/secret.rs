395bc07f6a72209325cbf34e585358da112b9f93
Shaun Taheri <shaun@blockjoy.com>
2025-02-07 14:32:12 +0000

    Rename grafana-basic-auth-key to grafana-loki-key

--- blockvisor-api/src/config/secret.rs
+++ blockvisor-api/src/config/secret.rs
@@ -7,27 +7,27 @@ use super::Redacted;
 
 const CLOUDFLARE_CERT_KEY_VAR: &str = "CLOUDFLARE_CERT_KEY";
 const CLOUDFLARE_CERT_KEY_ENTRY: &str = "secret.cloudflare_cert_key";
+const GRAFANA_LOKI_KEY_VAR: &str = "GRAFANA_LOKI_KEY";
+const GRAFANA_LOKI_KEY_ENTRY: &str = "secret.grafana_loki_key";
 const GRAFANA_PROMETHEUS_KEY_VAR: &str = "GRAFANA_PROMETHEUS_KEY";
 const GRAFANA_PROMETHEUS_KEY_ENTRY: &str = "secret.grafana_prometheus_key";
-const GRAFANA_BASIC_AUTH_KEY_VAR: &str = "GRAFANA_BASIC_AUTH_KEY";
-const GRAFANA_BASIC_AUTH_KEY_ENTRY: &str = "secret.grafana_basic_auth_key";
 
 #[derive(Debug, Display, Error)]
 pub enum Error {
     /// Failed to parse {CLOUDFLARE_CERT_KEY_ENTRY:?}: {0}
     CloudflareCertKey(provider::Error),
+    /// Failed to parse {GRAFANA_LOKI_KEY_ENTRY:?}: {0}
+    GrafanaLokiKey(provider::Error),
     /// Failed to parse {GRAFANA_PROMETHEUS_KEY_ENTRY:?}: {0}
     GrafanaPrometheusKey(provider::Error),
-    /// Failed to parse {GRAFANA_BASIC_AUTH_KEY_ENTRY:?}: {0}
-    GrafanaBasicAuthKey(provider::Error),
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
     pub cloudflare_cert_key: Redacted<String>,
+    pub grafana_loki_key: Redacted<String>,
     pub grafana_prometheus_key: Redacted<String>,
-    pub grafana_basic_auth_key: Redacted<String>,
 }
 
 impl TryFrom<&Provider> for Config {
@@ -37,17 +37,17 @@ impl TryFrom<&Provider> for Config {
         let cloudflare_cert_key = provider
             .read(CLOUDFLARE_CERT_KEY_VAR, CLOUDFLARE_CERT_KEY_ENTRY)
             .map_err(Error::CloudflareCertKey)?;
+        let grafana_loki_key = provider
+            .read(GRAFANA_LOKI_KEY_VAR, GRAFANA_LOKI_KEY_ENTRY)
+            .map_err(Error::GrafanaLokiKey)?;
         let grafana_prometheus_key = provider
             .read(GRAFANA_PROMETHEUS_KEY_VAR, GRAFANA_PROMETHEUS_KEY_ENTRY)
             .map_err(Error::GrafanaPrometheusKey)?;
-        let grafana_basic_auth_key = provider
-            .read(GRAFANA_BASIC_AUTH_KEY_VAR, GRAFANA_BASIC_AUTH_KEY_ENTRY)
-            .map_err(Error::GrafanaBasicAuthKey)?;
 
         Ok(Config {
             cloudflare_cert_key,
+            grafana_loki_key,
             grafana_prometheus_key,
-            grafana_basic_auth_key,
         })
     }
 }
